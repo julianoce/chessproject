e514da7bcbcd19de21d39663f3a96adba0eb5b56
brunosantos87 <brunopaivasantos87@gmail.com>
2018-10-29 15:58:35 -0300

    opções de jogo #22

--- xadrez/Assets/MenuController.cs
+++ xadrez/Assets/MenuController.cs
@@ -11,28 +11,30 @@ public class MenuController : MonoBehaviour
     public Text text_tutorial;
     public Text text_sair;
 
-    public Text checkW, checkB, backToMenu, jogar, facil, medio, dificil;
+    public Text checkW, checkB, backToMenu, jogar, facil, medio, dificil, backToMenuFromTutorial,
+                continuarJogo, sairJogo, tutorialJogo;
     public RawImage img_alto_falante;
     public RawImage img_sem_alto_falante;
-    bool som;
+    bool som, pause;
     Ray ray;
 
 	//level pode ser "facil", "medio" ou "dificil", black e white podem ser "player" ou "cpu". 
     public static string level, black, white;
-
+    Scene sc;
     RaycastHit hit;
     Camera cam;
     string menu;
     Animator anim;
 
 	
-    public GameObject canvasTutorial, canvasPrincipal, canvasNovoJogo;
+    public GameObject canvasTutorial, canvasPrincipal, canvasNovoJogo, canvasPause;
     // Use this for initialization
     void Start()
     {
-
+        sc = SceneManager.GetActiveScene();
         DontDestroyOnLoad(this.gameObject);
         som = true;
+        pause = false;
         anim = GetComponent<Animator>();
         //anim.SetBool("menuEnter", true);
         anim.Play("MenuAnimation");
@@ -41,18 +43,36 @@ public class MenuController : MonoBehaviour
         anim.SetBool("boardReturn", true);
         canvasNovoJogo.SetActive(false);
         canvasTutorial.SetActive(false);
+        canvasPause.SetActive(false);
         canvasPrincipal.SetActive(true);
         medio.fontStyle = FontStyle.Bold;
 		medio.fontSize = 17;
         level = "medio";
+        this.gameObject.SetActive(true);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        sc = SceneManager.GetActiveScene();
+        
 
+        if(sc.name == "Demo" && Input.GetKeyDown(KeyCode.Escape)){
+                pause = true;
+                this.gameObject.SetActive(true);
+                anim.SetBool("boardReturn", false);
+                anim.Play("MenuAnimation");
+                menu = "pause";
+                Debug.Log(Camera.allCameras);
+            }
 
+      
+            
+        
+
+       
+        
 
 
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -80,17 +100,45 @@ public class MenuController : MonoBehaviour
 
                 case "backToMenu":
                     backToMenu.fontStyle = FontStyle.Bold;
+                    jogar.fontStyle = FontStyle.Normal;
                     break;
 
 
                 case "jogar":
                     jogar.fontStyle = FontStyle.Bold;
+                    backToMenu.fontStyle = FontStyle.Normal;
                     break;
 
+                case "backToMenuTuto":
+                backToMenuFromTutorial.fontStyle = FontStyle.Bold;
+                jogar.fontStyle = FontStyle.Normal;
+                break;
+
+                case "continuarJogo":
+                continuarJogo.fontStyle = FontStyle.Bold;
+                tutorialJogo.fontStyle = FontStyle.Normal;
+                sairJogo.fontStyle = FontStyle.Normal;
+                break;
+
+                case "tutorialJogo":
+                continuarJogo.fontStyle = FontStyle.Normal;
+                tutorialJogo.fontStyle = FontStyle.Bold;
+                sairJogo.fontStyle = FontStyle.Normal;
+                break;
+
+                case "sairJogo":
+                continuarJogo.fontStyle = FontStyle.Normal;
+                tutorialJogo.fontStyle = FontStyle.Normal;
+                sairJogo.fontStyle = FontStyle.Bold;
+                break;
 
 
                 default:
 
+                    continuarJogo.fontStyle = FontStyle.Normal;
+                    tutorialJogo.fontStyle = FontStyle.Normal;
+                    sairJogo.fontStyle = FontStyle.Normal;
+                    backToMenuFromTutorial.fontStyle = FontStyle.Normal;
                     checkB.fontStyle = FontStyle.Normal;
                     backToMenu.fontStyle = FontStyle.Normal;
                     text_novo_jogo.fontStyle = FontStyle.Normal;
@@ -117,7 +165,7 @@ public class MenuController : MonoBehaviour
             if (Input.GetMouseButtonUp(0))
             {
 
-                Debug.Log(hit.collider.name);
+                
                 switch (hit.collider.name)
                 {
 
@@ -163,6 +211,12 @@ public class MenuController : MonoBehaviour
                         menu = "principal";
                         break;
 
+                    case "backToMenuTuto":
+                        anim.Play("MenuExitAnimation");
+                        menu = "principal";
+                        break;
+
+
                     case "jogar":
                         anim.SetBool("boardReturn", false);
                         anim.Play("MenuExitAnimation");
@@ -202,6 +256,27 @@ public class MenuController : MonoBehaviour
                         level = "dificil";
                         break;
 
+                //------------------------------PAUSE-----------------------------------
+                    case "continuarJogo":
+                    anim.SetBool("boardReturn", false);
+                    anim.Play("MenuExitAnimation");
+                    menu = "continuarJogo";
+                    pause =false;
+
+                    break;
+
+                    case "tutorialJogo":
+                        anim.SetBool("boardReturn", true);
+                        anim.Play("MenuExitAnimation");
+                        menu = "tutorial";
+                    break;
+
+                    case "sairJogo":
+                        menu = "principal";
+                        SceneManager.LoadScene("Menu");
+                    break;
+
+
                     default:
 
                         break;
@@ -210,10 +285,15 @@ public class MenuController : MonoBehaviour
 
         }
 
+        //----------------------------------PAUSE-----------------------------------
+
+       
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("trigger");
 
         switch (menu)
         {
@@ -221,21 +301,93 @@ public class MenuController : MonoBehaviour
                 canvasNovoJogo.SetActive(true);
                 canvasTutorial.SetActive(false);
                 canvasPrincipal.SetActive(false);
+                canvasPause.SetActive(false);
                 break;
 
             case "principal":
+                canvasPause.SetActive(false);
                 canvasNovoJogo.SetActive(false);
                 canvasTutorial.SetActive(false);
                 canvasPrincipal.SetActive(true);
 
                 break;
             case "tutorial":
-
+                canvasPause.SetActive(false);
                 canvasNovoJogo.SetActive(false);
                 canvasTutorial.SetActive(true);
                 canvasPrincipal.SetActive(false);
 
                 break;
+
+            case "pause":
+                canvasPause.SetActive(true);
+                canvasNovoJogo.SetActive(false);
+                canvasTutorial.SetActive(false);
+                canvasPrincipal.SetActive(false);
+            break;
+
+            case "continuarJogo":
+            Time.timeScale = 1;
+            this.gameObject.SetActive(false);
+
+            break;
+
+
+
+            default:
+
+
+                break;
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+
+    //----A FAZER!!!------------------------------canvas pause nao ta entrando!!!!!-canvas nao muda pq na outra cena nao tem o trigger changeMenu! -----
+    //---------------------------------ver posiçao da camera para para o pause!!!!! LEMBRAR DA CAMERA E CUBO DO GUIZAN (DEPTH ONLY E MEXER CULLING MASK [CRIAR LAYER DO MENU])
+    void OnTriggerExit(Collider other)
+    {
+         switch (menu)
+        {
+            case "novo jogo":
+                canvasNovoJogo.SetActive(true);
+                canvasTutorial.SetActive(false);
+                canvasPrincipal.SetActive(false);
+                canvasPause.SetActive(false);
+                break;
+
+            case "principal":
+                canvasPause.SetActive(false);
+                canvasNovoJogo.SetActive(false);
+                canvasTutorial.SetActive(false);
+                canvasPrincipal.SetActive(true);
+
+                break;
+            case "tutorial":
+                canvasPause.SetActive(false);
+                canvasNovoJogo.SetActive(false);
+                canvasTutorial.SetActive(true);
+                canvasPrincipal.SetActive(false);
+
+                break;
+
+            case "pause":
+                canvasPause.SetActive(true);
+                canvasNovoJogo.SetActive(false);
+                canvasTutorial.SetActive(false);
+                canvasPrincipal.SetActive(false);
+            break;
+
+            case "continuarJogo":
+            Time.timeScale = 1;
+            this.gameObject.SetActive(false);
+
+            break;
+
+
 
             default:
 
