a5ade933b859ef8120558b6aef91c577dd5841dc
carlos_hdc <carlos_hdc@hotmail.com>
2018-10-29 15:16:39 -0300

    Adicionando a classe para inicio da IA

---
+++ xadrez/Assets/Scripts/IA.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : MonoBehaviour {
+	//private BoardRules br;
+
+	void Start () {
+		//br = GameObject.FindObjectOfType(typeof(BoardRules)) as BoardRules;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
