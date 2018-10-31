d69c3f945e7c3a8e3842bc60dd26a95ad1116e21
carlos_hdc <carlos_hdc@hotmail.com>
2018-10-31 11:29:38 -0300

    Inicio do rascunho geral da classe

--- xadrez/Assets/Scripts/IA.cs
+++ xadrez/Assets/Scripts/IA.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IA : MonoBehaviour {
-	//private BoardRules br;
+	private BoardRules br;
+	private GameObject tabuleiro;
 
 	void Start () {
-		//br = GameObject.FindObjectOfType(typeof(BoardRules)) as BoardRules;
+		br = GameObject.FindObjectOfType(typeof(BoardRules)) as BoardRules;
+		//receber tabuleiro como parametro
+		//proxima_jogada = new List<>;
+		Random r = new Random();
+		max(tabuleiro, int.MinValue, int.MaxValue, 0)
+		//receber como parametro cor escolhida pelo jogador
 	}
 
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	int max(GameObject tab, int alpha, int beta, int poda){
+
+	}
+
+	int min(GameObject tab, int alpha, int beta, int poda){
+
+	}
+
+	List jogadas_possiveis(String cor){
+		//para cada peça da minha cor
+			//criar uma entrada na lista com a minha peça atual e a jogada possivel
 	}
 }
