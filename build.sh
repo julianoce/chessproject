3b775f4ee8d377d62a58059a51bae6a6c3c371c5
julianoce <julianoce@hotmail.com>
2018-11-02 22:25:28 -0300

    Travis Connection, one of the million trials

--- build.sh
+++ build.sh
@@ -1,49 +1,39 @@
 #! /bin/sh
 
+# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# Change this the name of your project. This will be the name of the final executables as well.
 project="chessproject"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
-  RSpec.configure do |c|
-    c.after :all do
-      Timecop.return
-    end
-  end
-
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
-
-
-echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
