2795da3c0b565a6d40ef2f410ccf929a50cc44b6
julianoce <julianoce@hotmail.com>
2018-11-11 18:52:05 -0200

    Limpeza na classe IA

--- xadrez/Assets/Scripts/IA.cs
+++ xadrez/Assets/Scripts/IA.cs
@@ -18,30 +18,14 @@ public class IA : MonoBehaviour {
 		// max(tabuleiro, int.MinValue, int.MaxValue, 0);	
 		// //receber como parametro cor escolhida pelo jogador
 	}
-		int MAX_PODAS = 6;
+		int MAX_PODAS = 3;
 	// Update is called once per frame
 	void Update () {
 		
 	}
 	public void olar(){
 		GameObject[][] tab = br.GetTabuleiro();
-		GameObject[][] tab_aux = criar(tab);
-		for(int i = 0; i < tab_aux.Length; i++) {
-			tab_aux[i] = new GameObject[8];
-			for(int j = 0; j < tab_aux[i].Length; j++) {
-				if (tab[i][j] && tab[i][j].name.StartsWith("Black")){
-					tab_aux[i][j] = pretas.transform.Find(tab[i][j].name).gameObject;
-				}
-			}
-		}
-		List<Vector2> l1 = br.MovimentosPossiveis(tab_aux,tab_aux[0][0]);
-		GameObject aux = tab_aux[0][0];
-		tab_aux[0][0]= tab_aux[0][1];
-		tab_aux[0][0] = aux;
-		List<Vector2> l2 = br.MovimentosPossiveis(tab_aux,tab_aux[0][0]);
-		if (!l1.Equals(l2)){
-			Debug.Log("FOI");
-		}
+		GameObject[][] tab_aux = copiar(tab);
 
 		Debug.Log("pqqqqqqqqqqqqqq");
 	}
@@ -58,10 +42,6 @@ public class IA : MonoBehaviour {
 
 	// }
 
-	// void jogadas_possiveis(String cor){
-	// 	//para cada peça da minha cor
-	// 		//criar uma entrada na lista com a minha peça atual e a jogada possivel
-	// }
 
 	private GameObject[][] criar(GameObject[][] tab){
 		GameObject[][] tab_n = new GameObject[8][];
@@ -71,17 +51,22 @@ public class IA : MonoBehaviour {
 		return tab_n;
 	}
 
-	// private void destruir(GameObject[][] tab_n){
-	// 	for(int i = 0; i < tab_n.Length; i++) {
-	// 		tab_n[i] = new GameObject[8];
-	// 		for(int j = 0; j < tab_n[i].Length; j++) {
-	// 			Debug.Log("entrou aqui?");
-	// 			if (tab_n[i][j]){
-	// 				Destroy(tab_n[i][j]);
-	// 			}
-	// 		}
-	// 	}
-	// }
+	private GameObject[][] copiar(GameObject[][] tab){
+		GameObject[][] tab_aux = criar(tab);
+		for(int i = 0; i < tab_aux.Length; i++) {
+			tab_aux[i] = new GameObject[8];
+			for(int j = 0; j < tab_aux[i].Length; j++) {
+				if (tab[i][j] && tab[i][j].name.StartsWith("Black")){
+					tab_aux[i][j] = pretas.transform.Find(tab[i][j].name).gameObject;
+				}
+				else if (tab[i][j] && tab[i][j].name.StartsWith("White")){
+					tab_aux[i][j] = brancas.transform.Find(tab[i][j].name).gameObject;
+				}
+			}
+		}
+		return tab_aux;
+	}
+
 	public int Utility(GameObject[][]tab){
 		string timeInimigo;
 		if(string.Equals(this.time,"White")){
