4f5443b12701764e73ad50a139517c47ba513d38
julianoce <julianoce@hotmail.com>
2018-11-11 15:31:43 -0200

    ia testing bug

--- xadrez/Assets/Scripts/IA.cs
+++ xadrez/Assets/Scripts/IA.cs
@@ -5,31 +5,36 @@ using UnityEngine;
 public class IA : MonoBehaviour {
 	private BoardRules br;
 	private GameObject tabuleiro;
+	private GameManager gm;
 
 	void Start () {
 		br = GameObject.FindObjectOfType(typeof(BoardRules)) as BoardRules;
-		//receber tabuleiro como parametro
-		//proxima_jogada = new List<>;
-		Random r = new Random();
-		max(tabuleiro, int.MinValue, int.MaxValue, 0)
-		//receber como parametro cor escolhida pelo jogador
+		gm = GameObject.FindObjectOfType(typeof(GameManager)) as GameManager;
+		// //receber tabuleiro como parametro
+		// //proxima_jogada = new List<>;
+		// Random r = new Random();
+		// max(tabuleiro, int.MinValue, int.MaxValue, 0);	
+		// //receber como parametro cor escolhida pelo jogador
 	}
 
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
-	int max(GameObject tab, int alpha, int beta, int poda){
-
+	public void olar(){
+		Debug.Log("pqqqqqqqqqqqqqq");
 	}
+		
+	// int max(GameObject tab, int alpha, int beta, int poda){
 
-	int min(GameObject tab, int alpha, int beta, int poda){
+	// }
 
-	}
+	// int min(GameObject tab, int alpha, int beta, int poda){
 
-	List jogadas_possiveis(String cor){
-		//para cada peça da minha cor
-			//criar uma entrada na lista com a minha peça atual e a jogada possivel
-	}
+	// }
+
+	// void jogadas_possiveis(String cor){
+	// 	//para cada peça da minha cor
+	// 		//criar uma entrada na lista com a minha peça atual e a jogada possivel
+	// }
 }
